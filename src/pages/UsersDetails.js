32d989550f230c4590936c2abb6733d2c19889e9
sarveshhome <sarvesh.carlton@gmail.com>
2021-12-17 18:04:39 +0000

    1 file modified

--- src/pages/UsersDetails.js
+++ src/pages/UsersDetails.js
@@ -8,20 +8,23 @@ const UsersDetails = (props) => {
   const id = locations.pathname.split('/')[2];
   console.log(JSON.stringify(props.data));
   const [data, setData] = useState(props.data);
-  const filterdata = data.filter((e) => {
-    return e.id == id;
-  });
+  const filterdata =
+    !data &&
+    data.filter((e) => {
+      return e.id == id;
+    });
   console.log(JSON.stringify(filterdata));
   return (
     <>
       <ul>
-        {filterdata.map((item, index) => (
-          <div>
-            <li key={index}>
-              <>{item.id}</> | <>{item.name}</> | <>{item.email}</>
-            </li>
-          </div>
-        ))}
+        {filterdata.length > 0 &&
+          filterdata.map((item, index) => (
+            <div>
+              <li key={index}>
+                <>{item.id}</> | <>{item.name}</> | <>{item.email}</>
+              </li>
+            </div>
+          ))}
       </ul>
     </>
   );
